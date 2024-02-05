eeb90aa240fca2645bbf960b60ec15899e7c9b7e
ValberJunior <valberjr1992@gmail.com>
2024-02-05 16:27:04 -0300

    add Docker

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 // /** @type {import('next').NextConfig} */
+
 // module.exports = {
 //   reactStrictMode: false,
 //   experimental: { appDir: true },
 //   typescript: { ignoreBuildErrors: true },
 // };
-/** @type {import('next').NextConfig} */
-module.exports = {
+
+const nextConfig = {
+  output: 'standalone',
   reactStrictMode: false,
   typescript: { ignoreBuildErrors: true },
-};
+}
+
+module.exports = nextConfig
